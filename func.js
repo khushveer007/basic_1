31adb7b32455d3b88a619565ae92a900ad26de3d
khushveer007 <122660325+khushveer007@users.noreply.github.com>
2024-08-31 11:44:23 +0530

    Create func.js

---
+++ func.js
@@ -0,0 +1,9 @@
+// scripts.js
+function showAlert() {
+    alert("Welcome to your fitness journey! Let's get started.");
+}
+
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('active');
+}
